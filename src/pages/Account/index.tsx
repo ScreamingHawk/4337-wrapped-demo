89af04dd095a0f0f3326ae74d8d463687047f8e8
Michael Standen <mstan@horizon.io>
2024-03-27 09:27:45 +1300

    Show signer key address

--- src/pages/Account/index.tsx
+++ src/pages/Account/index.tsx
@@ -34,11 +34,11 @@ export const AccountPage: React.FC = () => {
             You can claim more ETH from{" "}
             <Link href={CONFIG.FAUCET_URL}>a faucet</Link> if you need it.
           </p>
-          <Break />
         </>
       )}
       {signer && (
         <>
+          <Break />
           <H2>Signer Key</H2>
           <p>
             Your signer key is a private key that acts as the owner of your
@@ -46,10 +46,11 @@ export const AccountPage: React.FC = () => {
             behalf. Your signer <Accent>does not need to hold assets</Accent> in
             order to use ERC-4337 or ERC-5189.
             <br />
+            Your signer key address is <Accent>{signer.address}</Accent>.
+            <br />
             <strong>Do not share your signer key with anyone!</strong>
           </p>
           <ClickToShow>{signer?.privateKey}</ClickToShow>
-          <Break />
         </>
       )}
     </div>
